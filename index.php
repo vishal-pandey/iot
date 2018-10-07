c6f7dab7533b4a8c4a0396ea359caa2dfad2de56
System Administrator <root@VISHALs-MacBook-Pro.local>
2018-10-07 18:52:45 +0530

    db

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-	echo "Hello World";
+	$server = "localhost";
+	$user = "root";
+	$password = "12345";
+	$db = "iot";
+
+	$mysql = new mysql_connect($server, $user, $password, $db);
+	var_dump($mysql);
 ?>
